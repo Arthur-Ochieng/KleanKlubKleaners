0a5f672aa77b33ee7e53a22331977314eb1bf159
Arthur-Ochieng <johnarthur016@gmail.com>
2022-02-17 03:09:21 +0300

    updated with calendar screen

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatefulWidget {
+
+class CalendarPage extends StatelessWidget {
   const CalendarPage({ Key? key }) : super(key: key);
 
   @override
-  _CalendarPageState createState() => _CalendarPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const calendarPage(),
+      theme: ThemeData(
+        fontFamily: 'ubuntu'
+      ),
+    );
+  }
 }
 
-class _CalendarPageState extends State<CalendarPage> {
+class calendarPage extends StatefulWidget {
+  const calendarPage({ Key? key }) : super(key: key);
+
   @override
+  _calendarPageState createState() => _calendarPageState();
+}
+
+class _calendarPageState extends State<calendarPage> {
+  //CalendarController _calendarController = CalendarController();
+  @override
+  void initState(){
+    //TODO: Implement InitState
+    super.initState();
+  //  _calendarController = CalendarController();
+  }
+  void dispose(){
+    //TODO: Implement Dispose
+  //  _calendarController.dispose();
+    super.dispose();
+
+  }
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
       
     );
   }
