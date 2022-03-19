9171e521130c0a53be9234de157e67a6d1344885
Arthur-Ochieng <johnarthur016@gmail.com>
2022-03-19 19:51:06 +0300

    added cleaners page

--- lib/screens/fragments/details.dart
+++ lib/screens/fragments/details.dart
@@ -1,12 +1,323 @@
 import 'package:flutter/material.dart';
 
-class DetailsPage extends StatelessWidget {
+var serviceList = [
+  {'title': 'Carpet Cleaning', 'duration': 45, 'price': 30},
+  {'title': 'Floor Cleaning', 'duration': 45, 'price': 30},
+  {'title': 'House Dusting', 'duration': 45, 'price': 30},
+  {'title': 'Laundry', 'duration': 45, 'price': 30},
+];
 
+class DetailsPage extends StatelessWidget {
   final cleaner;
   DetailsPage(this.cleaner);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3 + 20,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    Image.asset(
+                      "assets/icons/clean.jpg",
+                      fit: BoxFit.fill,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      color: Colors.blueAccent.withOpacity(0.1),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 50,
+                left: 20,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height / 3 - 30,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 80,
+                        ),
+                        const Text(
+                          "Service List",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        ServiceTile(serviceList[0]),
+                        ServiceTile(serviceList[1]),
+                        ServiceTile(serviceList[2]),
+                        ServiceTile(serviceList[3]),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height / 8,
+                            color: const Color(0xff4E295B),
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    const Text(
+                                      "Angel Howard",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10,),
+                                    const Text(
+                                      'Mar 9 2022',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 30,),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Color(0xffFF8573),
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 2,),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Color(0xffFF8573),
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 2,),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Color(0xffFF8573),
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 2,),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Color(0xffFF8573),
+                                      size: 16,
+                                    ),
+                                    const SizedBox(width: 2,),
+                                    const Icon(
+                                      Icons.star,
+                                      color: Color(0xffFF8573),
+                                      size: 16,
+                                    ),
+                                  ],
+                                ),
+                                // SizedBox(height: 10,),
+                                // Text(
+                                //   'Cameron is the best colorist and stylish I have ever met. He is very talented and the best cleaner you could ask for',
+                                //   style: TextStyle(
+                                //     color: Colors.white,
+                                //     fontWeight: FontWeight.w300,
+                                //   ),
+                                // ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height / 3 - 120,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3 - 20,
+                        height: MediaQuery.of(context).size.height / 6 + 35,
+                        decoration: BoxDecoration(
+                          color: cleaner['bgColor'],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: <Widget>[
+                            Positioned(
+                              top: 10,
+                              right: -25,
+                              child: Image.asset(
+                                cleaner['imgUrl'],
+                                scale: 1.7,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              cleaner['cleanerName'],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              cleaner['company'],
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w300,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                const Icon(
+                                  Icons.star,
+                                  size: 16,
+                                  color: Color(0xffFF8573),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  cleaner['rating'],
+                                  style: const TextStyle(
+                                    color: Color(0xffFF8573),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '(${cleaner['rateAmount']})',
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 10,
+                top: MediaQuery.of(context).size.height / 3 - 55,
+                child: MaterialButton(
+                  onPressed: () {},
+                  padding: const EdgeInsets.all(10),
+                  shape: const CircleBorder(),
+                  color: Colors.white,
+                  child: const Icon(Icons.favorite_border),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ServiceTile extends StatelessWidget {
+  // const ServiceTile({Key? key}) : super(key: key);
+  final service;
+  ServiceTile(this.service);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2 - 40,
+                child: Text(
+                  service['title'],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                '${service['duration']} Min',
+                style: const TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            '\$${service['price']}',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {},
+            color: const Color(0xffFF8573),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Text(
+              'Book',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
